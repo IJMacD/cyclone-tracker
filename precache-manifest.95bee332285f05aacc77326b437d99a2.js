cfd481816b73a2b91d1a82694be4e25da1c5324c
Iain MacDonald <iain.ilearner@gmail.com>
2019-08-28 17:24:45 +0800

    Updates

---
+++ precache-manifest.95bee332285f05aacc77326b437d99a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bb93fb440129415c7876a938b9b069",
+    "url": "/cyclone-tracker/index.html"
+  },
+  {
+    "revision": "793de59adb64c4c0f5e6",
+    "url": "/cyclone-tracker/static/css/main.0ede9931.chunk.css"
+  },
+  {
+    "revision": "484d160429215f1d0b81",
+    "url": "/cyclone-tracker/static/js/2.c8f95aad.chunk.js"
+  },
+  {
+    "revision": "95f4c60d6303ee91dda4",
+    "url": "/cyclone-tracker/static/js/3.1293b57d.chunk.js"
+  },
+  {
+    "revision": "793de59adb64c4c0f5e6",
+    "url": "/cyclone-tracker/static/js/main.26eb55b8.chunk.js"
+  },
+  {
+    "revision": "f5813c33e6d6babffc55",
+    "url": "/cyclone-tracker/static/js/runtime~main.7d3a595c.js"
+  }
+]);
